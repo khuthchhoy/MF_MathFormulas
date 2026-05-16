72ab0d9ce50116f5dabf50a1ddf2b8168814912f
khuthchhoy <khuthchhoy@gmail.com>
2026-05-16 22:12:18 +0700

    update multiple files

--- worksheet_generator_registry.js
+++ worksheet_generator_registry.js
@@ -1,12 +1,12 @@
 
-const ProblemRegistry = {
-//   arithmetic,
-   algebra,
-//    trigonometry,
-   limit,
-//    derivative,
-//    integral,
-//    sequences,
-//    series
+window.ProblemRegistry = {
+    // arithmetic: window.arithmetic || { easy: [], med: [], hard: [] },
+    algebra: window.algebra || { easy: [], med: [], hard: [] },
+    // trigonometry: window.trigonometry || { easy: [], med: [], hard: [] },
+    limit: window.limit || { easy: [], med: [], hard: [] },
+    // derivative: window.derivative || { easy: [], med: [], hard: [] },
+    // integral: window.integral || { easy: [], med: [], hard: [] },
+    // sequences: window.sequences || { easy: [], med: [], hard: [] },
+    // series: window.series || { easy: [], med: [], hard: [] }
 };
 window.ProblemRegistry = ProblemRegistry;
